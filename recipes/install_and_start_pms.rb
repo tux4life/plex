27e550d1e54eb3411051a1754f085d5e19dac03f
Henry Muru Paenga <meringu@gmail.com>
2016-10-21 21:13:55 +1300

    refacotr into LWRP

---
+++ recipes/install_and_start_pms.rb
@@ -0,0 +1,3 @@
+plex_media_server 'pms' do
+  action :install_and_start
+end
---
+++ test/integration/uninstall/plex_spec.rb
@@ -0,0 +1,8 @@
+control 'Plex-plexmediaserver' do
+  impact 1.0
+  title 'The plexmediaserver service is running'
+
+  describe service('plexmediaserver') do
+    it { should_not be_installed }
+  end
+end
